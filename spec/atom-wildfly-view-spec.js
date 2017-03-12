7407c5dcec197e15de2ada3e29860832924e806d
Benjamin Flynn <bcflynn2@gmail.com>
2017-03-12 01:46:32 -0500

    This is an initial commit.
    These were made with the default structure and edited for some initial functionality.

---
+++ spec/atom-wildfly-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomWildflyView from '../lib/atom-wildfly-view';
+
+describe('AtomWildflyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
